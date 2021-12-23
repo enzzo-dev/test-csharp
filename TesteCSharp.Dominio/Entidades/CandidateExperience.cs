ab5c664091b324819b8f222be7bb26b87b39f514
enzzo-dev <enzzolimaalves2013@gmail.com>
2021-12-23 00:57:51 -0300

    Estrutura do projeto feita

---
+++ TesteCSharp.Dominio/Entidades/CandidateExperience.cs
@@ -0,0 +1,48 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TesteCSharp.Comum;
+using TesteCSharp.Dominio.Entidades;
+
+namespace TesteCSharp.Domains.Entidades
+{
+    public class CandidateExperience : Base
+    {
+
+        public CandidateExperience(string company, string job, string description, float salary, DateTime beginDate, Guid idCandidate)
+        {
+            AddNotifications(
+                new Contract<Notification>()
+                .Requires()
+                .IsNotEmpty(company, "Company", "The company of candidate could't be empty!")
+                .IsNotEmpty(job, "Job", "The job of candidate could't be empty!")
+                .IsNotEmpty(description, "Description", "The description of job could't be empty!")
+                .IsNotNull(salary, "Salary", "The salary of candidate could't be a null value!")
+                .IsNotNull(beginDate, "BeginDate", "Begin Date could't be a null value!")
+                .IsNotNull(idCandidate, "IdCandidate", "Candidate have to be specified!")
+            );
+
+            Company = company;
+            Job = job;
+            Description = description;
+            Salary = salary;
+            BeginDate = beginDate;
+            IdCandidate = idCandidate;
+        }
+
+        public string Company { get; private set; }
+        public string Job { get; private set; }
+        public string Description { get; private set; }
+        public float Salary { get; private set; }
+        public DateTime BeginDate { get; private set; }
+        public DateTime EndDate { get; set; }
+        public DateTime ModifyDate { get; private set; }
+
+        //Compositions
+        public Guid IdCandidate { get; set; }
+        public Candidates Candidates { get; set; }
+    }
+}
---
+++ TesteCSharp.Dominio/Entidades/Candidates.cs
@@ -0,0 +1,38 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteCSharp.Comum;
+using TesteCSharp.Domains;
+
+namespace TesteCSharp.Dominio.Entidades
+{
+   public class Candidates : Base
+    {
+        public Candidates(string name, string surname, DateTime birthdate, string email)
+        {
+            AddNotifications(
+                new Contract<Notification>()
+                .Requires()
+                .IsNotEmpty(name, "Name", "Name could't be empty!")
+                .IsNotEmpty(surname, "Surname", "Last name cannot be empty!")
+                .IsEmail(email, "Email", "The email format is incorrect!")
+                .IsNotNull(birthdate, "Birthdate", "The date of birth cannot be empty!")
+
+            );
+
+            Name = name;
+            Surname = surname;
+            Birthdate = birthdate;
+            Email = email;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public DateTime Birthdate { get; private set; }
+        public string Email { get; private set; }
+    }
+}
